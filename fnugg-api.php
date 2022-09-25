a1574b185dc59c0c58af7a6cb7f75dbf1a21c27c
omidkhaksar <mazaherkhaksar1994@gmail.com>
2022-09-25 10:52:31 +0330

    init comment

---
+++ fnugg-api.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       Fnugg Api
+ * Description:       Present data from a ski resort using the Fnugg API
+ * Requires at least: 5.9
+ * Requires PHP:      7.0
+ * Version:           1.0.0
+ * Author:            Mazaher Khaksar
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       fnugg-api
+ *
+ * @package           create-block
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_fnugg_api_block_init() {
+	register_block_type( __DIR__ . '/build' );
+}
+add_action( 'init', 'create_block_fnugg_api_block_init' );
